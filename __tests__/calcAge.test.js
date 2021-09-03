20c264467d5d2bea68c1839228d10d3fa4e6e28a
Roman Kolivashko <rkolivashko@gmail.com>
2021-09-03 10:21:06 -0700

    Numerc value return of a dob year RED test

--- __tests__/calcAge.test.js
+++ __tests__/calcAge.test.js
@@ -2,9 +2,17 @@ import CalcAge from '../src/js/calcAge.js';
 
 describe('calcAge', () => {
 
+  let age;
+  beforeEach(() => {
+    age = new CalcAge('1956-9-1');
+  });
+
   test('This should create a dob object', () => {
-    let age = new CalcAge('1956-9-1');
     expect(age.dob).toBe('1956-9-1');
+  });
+
+  test('This should return current age on Earth', () => {
+    expect(age.getEarthAge).toEqual(65);
   });
 
 });
--- src/js/calcAge.js
+++ src/js/calcAge.js
@@ -5,5 +5,9 @@ export default class calcAge {
     this.dob = dob;
   }
 
+  getEarthAge() {
+
+  }
+
 }
 
